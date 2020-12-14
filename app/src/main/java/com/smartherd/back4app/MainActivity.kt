b7a7fe3e80f1b825df4feb96f9502bfc2f8f225f
Godwill Junior <jamesgodwillarkoh@gmail.com>
2020-12-14 20:32:07 +0000

    Added Toast in work area

--- app/src/main/java/com/smartherd/back4app/MainActivity.kt
+++ app/src/main/java/com/smartherd/back4app/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.smartherd.back4app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "Hello Everyone", Toast.LENGTH_LONG).show()
     }
 }
